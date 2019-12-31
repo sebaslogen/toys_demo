a9e3920c88451069174b3b966ab905e45430bcdc
sebaslogen <sebastian@q42.nl>
2019-12-31 16:11:24 +0100

    0-intro: Add hello world

---
+++ lib/home/ToysHomePage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class ToysHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Toys Home"),
+        ),
+        body: Center(child: Text("Hola mundo")));
+  }
+}
