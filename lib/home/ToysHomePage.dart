833f348c8e2029bd08bba934e39def2b094ca56d
sebaslogen <sebastian@q42.nl>
2020-01-02 17:56:41 +0100

    1-basic-widgets: remove app bar & add column with safe area

--- lib/home/ToysHomePage.dart
+++ lib/home/ToysHomePage.dart
@@ -5,21 +5,23 @@ class ToysHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Toys Home"),
-        ),
-        body: Center(
-            child: Container(
-          decoration: BoxDecoration(
-              color: Colors.lightBlueAccent,
-              borderRadius: BorderRadius.only(
-                  topRight: Radius.circular(10),
-                  bottomRight: Radius.circular(10))),
-          padding: EdgeInsets.all(10),
-          child: Text(
-            "Hola mundo",
-            style: Theme.of(context).textTheme.title,
+        body: SafeArea(
+      child: Column(
+        children: <Widget>[
+          Container(
+            decoration: BoxDecoration(
+                color: Colors.lightBlueAccent,
+                borderRadius: BorderRadius.only(
+                    topRight: Radius.circular(10),
+                    bottomRight: Radius.circular(10))),
+            padding: EdgeInsets.all(10),
+            child: Text(
+              "Hola mundo",
+              style: Theme.of(context).textTheme.title,
+            ),
           ),
-        )));
+        ],
+      ),
+    ));
   }
 }
