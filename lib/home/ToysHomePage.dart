c43ddea248d2a606d8d44b93c16cf6505c2f5ddc
sebaslogen <sebastian@q42.nl>
2020-01-03 10:14:04 +0100

    2-image-loading: Add cached network image library and load background image

--- lib/home/ToysHomePage.dart
+++ lib/home/ToysHomePage.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
@@ -7,29 +8,39 @@ class ToysHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SafeArea(
-      child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: <Widget>[
-              Text(
-                "Discover this year's hottest toys",
-                style: Theme.of(context).textTheme.display1,
+        body: Stack(
+          children: <Widget>[
+            CachedNetworkImage(
+              width: double.infinity,
+              height: double.infinity,
+              fit: BoxFit.cover,
+              imageUrl: 'https://cdn.pixabay.com/photo/2016/10/29/10/13/abstract-1780373_1280.png',
+            ),
+            SafeArea(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: SingleChildScrollView(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Text(
+                        "Discover this year's hottest toys",
+                        style: Theme.of(context).textTheme.display1,
+                      ),
+                      SizedBox(height: 20,),
+                      SearchBox(),
+                      SizedBox(height: 20,),
+                      Container(height: 300, color: Colors.purple,),
+                      SizedBox(height: 20,),
+                      Container(height: 400, color: Colors.orange,),
+                      SizedBox(height: 20,),
+                      Container(height: 300, color: Colors.blue,),
+                    ],
+                  ),
+                ),
               ),
-              SizedBox(height: 20,),
-              SearchBox(),
-              SizedBox(height: 20,),
-              Container(height: 300, color: Colors.purple,),
-              SizedBox(height: 20,),
-              Container(height: 400, color: Colors.orange,),
-              SizedBox(height: 20,),
-              Container(height: 300, color: Colors.blue,),
-            ],
-          ),
-        ),
-      ),
-    ));
+            )
+          ],
+        ));
   }
 }
