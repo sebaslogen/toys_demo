64aeeb2393d33917548269a5b3e9739f548107a8
sebaslogen <sebastian@q42.nl>
2020-01-06 17:47:14 +0100

    5-list-widgets: add first toy mock data

--- lib/data.dart
+++ lib/data.dart
@@ -16,3 +16,8 @@ class Toy {
       @required this.price,
       @required this.popularityRank});
 }
+
+// Mock data instead of using a backend
+final toysData = [
+  Toy(name: 'Robot Podolski', description: 'Awesome autonomous robot', imageName: 'podolski', hexColorBackground: 0xFEBF54, price: 87.99, popularityRank: 136),
+];
