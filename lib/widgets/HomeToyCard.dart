9ea3605b6f487a3fa6f58c3df3eb4dadbdc78565
sebaslogen <sebastian@q42.nl>
2020-01-14 17:38:58 +0100

    5-list-widgets: add home toy card

--- lib/widgets/HomeToyCard.dart
+++ lib/widgets/HomeToyCard.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/widgets.dart';
 
 import '../data.dart';
@@ -12,6 +13,22 @@ class HomeToyCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text('Row: ${toyData.name}');
+    var color = Color(toyData.hexColorBackground);
+    return Container(
+      margin: EdgeInsets.all(12.0),
+        decoration: BoxDecoration(color: color,
+            borderRadius: BorderRadius.circular(15),
+            boxShadow: [
+          BoxShadow(
+            color: color,
+            offset: Offset(0, 4.0),
+            blurRadius: 7.0,
+            spreadRadius: 1.0,
+          )
+        ]),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text('Row: ${toyData.name}'),
+        ));
   }
 }
