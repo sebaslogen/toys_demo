41797007e620d767cb375906a153d741cfa1a168
sebaslogen <sebastian@q42.nl>
2020-01-14 20:47:12 +0100

    5-list-widgets: Add image to toys card

--- lib/widgets/HomeToyCard.dart
+++ lib/widgets/HomeToyCard.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 
 import '../data.dart';
@@ -15,20 +15,30 @@ class HomeToyCard extends StatelessWidget {
   Widget build(BuildContext context) {
     var color = Color(toyData.hexColorBackground);
     return Container(
-      margin: EdgeInsets.all(12.0),
-        decoration: BoxDecoration(color: color,
+        margin: EdgeInsets.all(12.0),
+        decoration: BoxDecoration(
+            color: color,
             borderRadius: BorderRadius.circular(15),
             boxShadow: [
-          BoxShadow(
-            color: color,
-            offset: Offset(0, 4.0),
-            blurRadius: 7.0,
-            spreadRadius: 1.0,
-          )
-        ]),
+              BoxShadow(
+                color: color,
+                offset: Offset(0, 4.0),
+                blurRadius: 7.0,
+                spreadRadius: 1.0,
+              )
+            ]),
         child: Padding(
           padding: const EdgeInsets.all(8.0),
-          child: Text('Row: ${toyData.name}'),
+          child: Column(
+            children: <Widget>[
+              Image.asset("assets/images/${toyData.imageName}.png",
+                  width: 100, height: 200, alignment: Alignment.bottomCenter),
+              SizedBox(
+                height: 20,
+              ),
+              Text(toyData.name, style: Theme.of(context).textTheme.title,),
+            ],
+          ),
         ));
   }
 }
