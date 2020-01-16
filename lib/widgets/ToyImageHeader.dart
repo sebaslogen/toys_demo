ceb8fc80a1fbcacbf53391df136d8c63f8a275d2
sebaslogen <sebastian@q42.nl>
2020-01-16 10:22:09 +0100

    7-animations-3-tween-animation-builder: add Color tint slider

--- lib/widgets/ToyImageHeader.dart
+++ lib/widgets/ToyImageHeader.dart
@@ -16,24 +16,54 @@ class ToyImageHeader extends StatefulWidget {
 }
 
 class _ToyImageHeaderState extends State<ToyImageHeader> {
-  var _tintColor = Colors.white;
+  final _tintColors = [
+    NamedColor('White', Colors.white),
+    NamedColor('Blue', Colors.blue),
+    NamedColor('Red', Colors.red),
+    NamedColor('Purple', Colors.deepPurpleAccent)
+  ];
+  var _tintColorIndex = 0;
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
-      child: Hero(
-        tag: widget.toy.imageName,
-        child: TweenAnimationBuilder(
-            tween: ColorTween(begin: Colors.white, end: Colors.red),
-            duration: Duration(seconds: 2),
-            builder: (_, Color color, __) {
-              return ColorFiltered(
-                colorFilter: ColorFilter.mode(color, BlendMode.modulate),
-                child: Image.asset("assets/images/${widget.toy.imageName}.png", height: 300, alignment: Alignment.bottomCenter),
-              );
-            }),
+      child: Column(
+        children: <Widget>[
+          Hero(
+            tag: widget.toy.imageName,
+            child: TweenAnimationBuilder(
+                tween: ColorTween(begin: Colors.white, end: _tintColors[_tintColorIndex].color),
+                duration: Duration(seconds: 2),
+                builder: (_, Color color, __) {
+                  return ColorFiltered(
+                    colorFilter: ColorFilter.mode(color, BlendMode.modulate),
+                    child: Image.asset("assets/images/${widget.toy.imageName}.png", height: 300, alignment: Alignment.bottomCenter),
+                  );
+                }),
+          ),
+          Slider(
+            value: _tintColorIndex.ceilToDouble(),
+            onChanged: (newVal) {
+              setState(() {
+                _tintColorIndex = newVal.floor();
+              });
+            },
+            activeColor: _tintColors[_tintColorIndex].color,
+            min: 0,
+            max: _tintColors.length.ceilToDouble() - 1,
+            divisions: _tintColors.length - 1,
+            label: "${_tintColors[_tintColorIndex].name}",
+          ),
+        ],
       ),
     );
   }
+}
+
+class NamedColor {
+  final Color color;
+  final String name;
+
+  NamedColor(this.name, this.color);
 }
